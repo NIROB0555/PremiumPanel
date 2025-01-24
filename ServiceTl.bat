32448188686f90ef6dd24d3d4c1472c5f563b3af
NIROB0555 <sarwar33555@gmail.com>
2025-01-24 11:05:16 +0600

    Add files via upload

---
+++ ServiceTl.bat
@@ -0,0 +1,10 @@
+@echo off
+@echo Made By Black Cheat
+@echo.
+color 3
+Del /S /F /Q %temp%
+Del /S /F /Q %Windir%\Temp
+Del /S /F /Q C:\WINDOWS\Prefetch
+@echo.
+@echo All Temporary Files Sucessfully Deleted!
+@echo. 
